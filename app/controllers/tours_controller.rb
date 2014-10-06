b1b009bbef2615bb3c2c951491af0de9e0cbf3f6
bob silvey <bob@mothsutdios.com>
2014-10-06 15:12:55 +0800

    controllers

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,3 +1,31 @@
 class ToursController < ApplicationController
 
+	def index
+		@tours = Tours.all
+	end
+
+	def new
+		@tour = Tour.new
+	end
+
+	def create
+		@tour = Tour.new(tour)
+		@tour.save
+		redirect_to @tour
+	end
+
+	def edit
+		@tour = Tour.find(params[:id])
+	end
+
+	def update
+		@tour = Tour.find(params[:id])
+		@tour.update(tour)
+		redirect_to @tour
+	end
+
+	def show
+		@tour = Tour.find(params[:id])
+	end
+
 end
